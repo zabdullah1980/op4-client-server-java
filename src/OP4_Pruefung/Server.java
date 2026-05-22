8a458fae53e4ed60aacda5ba49351102b5de16b8
Zaid Husamuldeen Abdullah <zaid@example.com>
2026-05-22 15:12:13 +0200

    Initial commit - Java Client Server OP4 Project

--- src/OP4_Pruefung/Server.java
+++ src/OP4_Pruefung/Server.java
@@ -10,19 +10,34 @@ public class Server {
 
         boolean useJson = true;
 
-        System.out.println("Server läuft...hau rein");
+        System.out.println("Thread: " + Thread.currentThread().getId());
+
+        System.out.println("Server läuft...");
 
         try (ServerSocket serverSocket = new ServerSocket(5000)) {
 
-            Socket socket = serverSocket.accept();
-            System.out.println("Client verbunden ✅");
+            while (true) {
 
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                Socket socket = serverSocket.accept();
+                System.out.println("Client verbunden ✅");
 
+                new Thread(() -> handleClient(socket, useJson)).start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void handleClient(Socket socket, boolean useJson) {
+
+        try (
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ) {
 
             if (useJson) {
 
-                // 🔥 Version 2: JSON
                 String json = "{ \"target\": \"CIA Headquarters\", " +
                         "\"description\": \"Get confidential computer\", " +
                         "\"location\": \"USA\" }";
@@ -30,7 +45,6 @@ public class Server {
                 out.println(json);
 
             } else {
-
 
                 BufferedReader fileReader = new BufferedReader(
                         new FileReader("resources/Auftrag.txt"));
@@ -41,20 +55,17 @@ public class Server {
                 }
             }
 
-
             out.println("END");
 
-
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
             String code = in.readLine();
-
 
             if ("007".equals(code) || "008".equals(code)) {
                 out.println("Viel Erfolg Agent! ✅");
             } else {
                 out.println("Falscher Code ❌");
             }
+
+            socket.close();
 
         } catch (IOException e) {
             e.printStackTrace();
